2063fe589e4a30e77a4fdab9d2bd1eeab3a6fe42
yarfed <yarfed@yandex.ru>
2018-03-10 01:15:51 +0200

    add dbl packadge create bd

--- src/main/java/telb1/dbl/DbManager.java
+++ src/main/java/telb1/dbl/DbManager.java
@@ -1,7 +1,19 @@
 package telb1.dbl;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  * Created by User on 10.03.2018.
  */
 public class DbManager {
+    public void connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
