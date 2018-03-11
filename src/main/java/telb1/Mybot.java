5ed864d82befc5e10350b1e724f7295c14de750d
yarfed <yarfed@yandex.ru>
2018-03-11 20:15:54 +0200

    add .gitignore add config pass generator for cars ready washers signUp ready check carPassword

--- src/main/java/telb1/Mybot.java
+++ src/main/java/telb1/Mybot.java
@@ -14,6 +14,7 @@ import telb1.dbl.DbManager;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.sql.SQLException;
 
 
 public class Mybot extends TelegramLongPollingBot {
@@ -68,14 +69,25 @@ public class Mybot extends TelegramLongPollingBot {
                 sendMessage(carPass, chatId);
                 break;
             case "carPass":
-
+                DbManager.INSTANCE.cleanExpiredPasswords();
+                carId = DbManager.INSTANCE.getCarIdFromPasswords(messageText);
+                if (carId == null) {
+                    sendMessage("wrong or expired password", chatId);
+                    break;
+                }
+                sendMessage("very good car password" + messageText, chatId);
                 break;
             case "washerPass":
                 if (!DbManager.INSTANCE.checkWasherPassword(messageText)) {
                     sendMessage("wrong pass" + messageText, chatId);
                     break;
                 }
-                
+                try {
+                    DbManager.INSTANCE.washerRegistration(messageText, chatId);
+                    sendMessage("you in" + messageText, chatId);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e.getMessage());
+                }
                 break;
 
             default:
@@ -104,10 +116,6 @@ public class Mybot extends TelegramLongPollingBot {
 
     private boolean isAdmin(Long chatId) {
         return Config.INSTANCE.ADMIN_CHAT_ID.contains(chatId.toString());
-    }
-
-    private String generatePass(String messageText) {
-        return "1010";
     }
 
 }
--- src/main/java/telb1/dbl/DbManager.java
+++ src/main/java/telb1/dbl/DbManager.java
@@ -41,26 +41,27 @@ public class DbManager {
     }
 
     public String createCarPass(Integer carId) {
-        String carPass=null;
+        String carPass = null;
         try (Connection connection = DriverManager.getConnection(Config.INSTANCE.DATABASE_URL)) {
             PreparedStatement preparedStatement = connection.prepareStatement(
                     "INSERT INTO passwords(car_id,password,datetime) values(?,?,?)");
             carPass = String.format("%04d", new Random().nextInt(10000));
             preparedStatement.setInt(1, carId);
-            preparedStatement.setString(2, "p"+carPass);
+            preparedStatement.setString(2, "p" + carPass);
             preparedStatement.setDate(3, new java.sql.Date(System.currentTimeMillis()));
-           preparedStatement.executeUpdate();
+            preparedStatement.executeUpdate();
             return carPass;
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
 
     }
-    public void cleanExpiredPasswords(){
+
+    public void cleanExpiredPasswords() {
         try (Connection connection = DriverManager.getConnection(Config.INSTANCE.DATABASE_URL)) {
             PreparedStatement preparedStatement = connection.prepareStatement(
                     "DELETE FROM passwords WHERE datetime < ?");
-            preparedStatement.setDate(1, new java.sql.Date(System.currentTimeMillis()-180000));
+            preparedStatement.setDate(1, new java.sql.Date(System.currentTimeMillis() - 180000));
             preparedStatement.executeUpdate();
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
@@ -69,7 +70,6 @@ public class DbManager {
 
     public boolean checkWasherPassword(String password) {
 
-
         try (Connection connection = DriverManager.getConnection(Config.INSTANCE.DATABASE_URL)) {
             PreparedStatement preparedStatement = connection.prepareStatement(
                     "SELECT COUNT(*) AS total FROM washers where password=?");
@@ -77,11 +77,60 @@ public class DbManager {
             preparedStatement.setString(1, password);
             ResultSet rs = preparedStatement.executeQuery();
 
-            while(rs.next()){
-                if (rs.getInt("total")==0) return false;
+            while (rs.next()) {
+                if (rs.getInt("total") == 0) return false;
             }
 
             return true;
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public void washerRegistration(String washerPassword, Long chatId) throws SQLException {
+
+        Connection connection = null;
+        try {
+            connection=DriverManager.getConnection(Config.INSTANCE.DATABASE_URL);
+            connection.setAutoCommit(false);
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "UPDATE washers SET chat_id = null WHERE chat_id=?");
+
+            preparedStatement.setLong(1, chatId);
+            preparedStatement.executeUpdate();
+            preparedStatement = connection.prepareStatement(
+                    "UPDATE washers SET chat_id = ? WHERE password=?");
+            preparedStatement.setLong(1, chatId);
+            preparedStatement.setString(2, washerPassword);
+            preparedStatement.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            if (connection != null) {
+                connection.rollback();
+            }
+
+            throw new RuntimeException(e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    public Integer getCarIdFromPasswords(String carPassword) {
+        Integer carId=null;
+        try (Connection connection = DriverManager.getConnection(Config.INSTANCE.DATABASE_URL)) {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "SELECT * FROM passwords where password=?");
+
+            preparedStatement.setString(1, "p"+carPassword);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            if (rs.next()) {
+                carId = rs.getInt("car_id");
+            }
+
+            return carId;
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
